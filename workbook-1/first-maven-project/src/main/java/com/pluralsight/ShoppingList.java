c0cd96580fdc70da9c174f9c419151e41a165724
Jacob Lockhart <procoder165@gmail.com>
2024-09-26 10:28:40 -0400

    fixing ShoppingList

--- workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -1,17 +1,18 @@
 package com.pluralsight;
 
 public class ShoppingList {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Milk");
+        System.out.println("Cereal");
+        System.out.println("Eggs");
+        System.out.println("Water");
+        System.out.println("Bacon");
+        System.out.println("Tea");
+        System.out.println("C4");
+        System.out.println("Cottage Cheese");
+        System.out.println("Oats");
+        System.out.println("LF Cheese");
+        System.out.println("Lean Ground Beef");
     }
 }
