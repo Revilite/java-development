c0dbaf0a135569086efebc76471ad63ea482f155
Jacob Lockhart <procoder165@gmail.com>
2024-10-01 10:03:07 -0400

    adding comments for readability

--- workbook-1/financial-calculator/src/main/java/com/pluralsight/financialCalculator/FinancialCalculator.java
+++ workbook-1/financial-calculator/src/main/java/com/pluralsight/financialCalculator/FinancialCalculator.java
@@ -1,17 +1,10 @@
 package com.pluralsight.financialCalculator;
-
 import java.util.Scanner;
+
 //init variables as doubles
 /*
-     TODO: Create a calculator that determines present value of a ordinary annuity (WHAT???)
-        would accept monthly payout, expected interest rate, and years to payout
-        display present value of that annuity (WHAT X2 ?????)
-
-
         Margin of Overflow error
-
-
- */
+*/
 
 public class FinancialCalculator {
 
@@ -22,6 +15,7 @@ public class FinancialCalculator {
         double monthlyInterestRate = interestRate / 12;
         int months = years * 12;
 
+        //Monthly Compounding Formula
         monthlyPayment = principal * ((monthlyInterestRate) / (1 - Math.pow((1 + monthlyInterestRate), -1 * months)));
         totalInterest = monthlyPayment * months - principal;
 
@@ -33,9 +27,9 @@ public class FinancialCalculator {
         // Initial interest rate is yearly
         double futureValue = 0;
         double totalInterest = 0;
-
         int daily = years * 365;
 
+        //Daily Compounding formula
         futureValue = deposit * (Math.pow(1 + interestRate / 365, daily));
         totalInterest = futureValue - deposit;
 
@@ -49,6 +43,7 @@ public class FinancialCalculator {
         int numOfPeriods = years * 12;
         double monthlyInterest = interestRate / 12;
 
+        //Annuity for present value formula
         presentValue = monthlyPayout * ((1 - (Math.pow(1 + monthlyInterest, -numOfPeriods))) / monthlyInterest);
 
         return presentValue;
@@ -57,47 +52,54 @@ public class FinancialCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String calculatorChoice = "";
+        String continueLoop = "y";
+        while(continueLoop.contains("y")) {
+            System.out.println("Which calculator would you like you use?   mc/fv/pv");
+            calculatorChoice = scan.next();
 
-        System.out.println("Which calculator would you like you use?   mc/fv/pv");
-        calculatorChoice = scan.next();
 
+            if (calculatorChoice.contains("mc")) {
+                // User Input
+                System.out.println("What is your principal?");
+                double principal = scan.nextDouble();
+                System.out.println("What is you interest rate? (As percentage)");
+                double interestRate = scan.nextDouble() / 100;
+                System.out.println("How many years is the loan for?");
+                int years = scan.nextInt();
 
-        if (calculatorChoice.contains("mc")) {
-            System.out.println("What is your principal?");
-            double principal = scan.nextDouble();
-            System.out.println("What is you interest rate? (As percentage)");
-            double interestRate = scan.nextDouble() / 100;
-            System.out.println("How many years is the loan for?");
-            int years = scan.nextInt();
-            double[] mortgageValues = mortgageCalculator(principal, interestRate, years);
+                //Spitting output
+                double[] mortgageValues = mortgageCalculator(principal, interestRate, years);
+                System.out.printf("your monthly payment is $%.0f \n", mortgageValues[0]);
+                System.out.printf("your total amount of interest is $%.2f", mortgageValues[1]);
+            } else if (calculatorChoice.contains("fv")) {
+                // User Input
+                System.out.println("What is your deposit?");
+                double deposit = scan.nextDouble();
+                System.out.println("What is your interest? (As percentage)");
+                double interest = scan.nextDouble() / 100;
+                System.out.println("How many years?");
+                int years = scan.nextInt();
 
-            System.out.println(mortgageValues[0]);
-            System.out.printf("your monthly payment is $%.0f \n", mortgageValues[0]);
-            System.out.printf("your total amount of interest is $%.2f", mortgageValues[1]);
-        } else if (calculatorChoice.contains("fv")) {
-            System.out.println("What is your deposit?");
-            double deposit = scan.nextDouble();
-            System.out.println("What is your interest? (As percentage)");
-            double interest = scan.nextDouble() / 100;
-            System.out.println("How many years?");
-            int years = scan.nextInt();
+                //Spitting output
+                double[] futureValue = futureValueCalculator(deposit, interest, years);
+                System.out.printf("your future value is $%.2f \n", futureValue[0]);
+                System.out.printf("your total amount of interest is $%.2f", futureValue[1]);
+            } else if (calculatorChoice.contains("pv")) {
+                // User Input
+                System.out.println("What is your monthly payout?");
+                double monthly = scan.nextDouble();
+                System.out.println("What is your expected interest rate? (As percentage)");
+                double interestRate = scan.nextDouble() / 100;
+                System.out.println("How many years is it going to take to payout?");
+                int years = scan.nextInt();
 
-            double[] futureValue = futureValueCalculator(deposit, interest, years);
-            System.out.printf("your future value is $%.2f \n", futureValue[0]);
-            System.out.printf("your total amount of interest is $%.2f", futureValue[1]);
-        } else if (calculatorChoice.contains("pv")) {
+                //Spitting output
+                double presentValue = presentValueCalculator(monthly, interestRate, years);
+                System.out.printf("The present value that you would have to pay is $%.2f", presentValue);
+            }
 
-            System.out.println("What is your monthly payout?");
-            double monthly = scan.nextDouble();
-            System.out.println("What is your expected interest rate? (As percentage)");
-            double interestRate = scan.nextDouble() / 100;
-            System.out.println("How many years is it going to take to payout?");
-            int years = scan.nextInt();
-
-            double presentValue = presentValueCalculator(monthly, interestRate, years);
-
-            System.out.printf("The present value that you would have to pay is $%.2f", presentValue);
+            System.out.println("\nWould you like to use another calculation?  (y/n)");
+            continueLoop = scan.next();
         }
-
     }
 }
